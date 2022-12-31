8f241f348d5eadb7f522349c6e540824eee42d21
Ramya Sirimalla <98519460+Laxmanramya@users.noreply.github.com>
2022-12-31 14:29:59 +0530

    Add files via upload

---
+++ logic.js
@@ -0,0 +1,7 @@
+function changeColor() {
+
+    document.getElementById("demo").style.fontSize = '25px';
+    document.getElementById("demo").style.color = 'red';
+    document.getElementById("color").style.backgroundColor = 'blue';
+    document.getElementById("changecolor").style.color = 'white';
+}
